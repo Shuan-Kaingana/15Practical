bd1f83edfc95879b437c114c03bff155feea0d3c
Shuan-Kaingana <4558817@myuwc.ac.za>
2026-03-09 15:09:16 +0200

    Starting to Code

---
+++ Anagrams.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Anagrams{
+    public static void main(String[]args){
+        try{
+            BufferedReader reader = new BufferedReader(System FileReader("ULYSSES by James Joyce, 1922.txt"));
+        }
+        catch(e){
+            e.printStack
+
+        }
+
+        
+
+    }
+}
